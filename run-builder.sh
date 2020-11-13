5884808c66d4aa0535752d3c54ad4386148f30cd
a-carlosai <ailton_hc.carlos@hotmail.com>
2020-11-13 00:18:19 -0300

    first commit

--- run-builder.sh
+++ run-builder.sh
@@ -32,7 +32,6 @@ ${GCLOUD} compute instances create \
        --zone=${ZONE} --subnet=${SUBNET} --network-tier=${INSTANCE_ARGS} ${INSTANCE_NAME} \
        --metadata block-project-ssh-keys=TRUE \
        --metadata-from-file ssh-keys=ssh-keys \
-       ${INSTANCE_NAME}
 
 trap cleanup EXIT
 
